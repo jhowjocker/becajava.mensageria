ce2139f9a216def832c0dd003746d0b7fa84b8f8
Jonathan Rodrigues Santos <jrsantos@everis.com>
2020-11-18 01:04:31 -0300

    HelloWorld Mensageria

---
+++ src/main/java/br/helloworld/app/mensageria/Enviador.java
@@ -0,0 +1,26 @@
+package br.helloworld.app.mensageria;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+public class Enviador {
+
+	private final static String QUEUE_NAME = "exemplo";
+
+	public static void main(String[] argv) throws Exception {
+	      
+		  ConnectionFactory factory = new ConnectionFactory();
+		  factory.setHost("localhost");
+		  try (Connection connection = factory.newConnection();
+		       Channel channel = connection.createChannel()){
+		    	   
+		    	   
+		    	   channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		    	   String message = "Hello World!";
+		    	   channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+		    	   System.out.println("Send '" + message + "'");
+		       }
+	  }
+
+}
